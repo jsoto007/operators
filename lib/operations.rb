10af6f72eda6c04f8bdd57b662c69fd04ee4fa89
Jose Soto <jsoto7087@gmail.com>
2021-01-17 12:11:35 -0500

    commit tree 875c571656f5c35c3f2e160b53f74c4ffcdab6c6

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
-def unsafe?(speed)
 
+def unsafe?(speed)
+if speed < 40 || speed > 60
+    return true
+end
+return false
 end
 
 
 
 def not_safe?(speed)
-	
+    speed < 40 || speed > 60 ? true : false
+
 end
-	
+
+
+
 
 
